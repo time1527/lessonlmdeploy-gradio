f75deb8a85973d78ef16e05c1cba93d512611402
time1527 <154412155+time1527@users.noreply.github.com>
2024-04-19 15:24:42 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# https://github.com/InternLM/lmdeploy/blob/main/docs/zh_cn/serving/gradio.md
+import gradio as gr
+from lmdeploy import pipeline, TurbomindEngineConfig,GenerationConfig
+
+backend_config = TurbomindEngineConfig(cache_max_entry_count=0.01)
+
+# download internlm2 to the base_path directory using git tool
+base_path = './lesson-lmdeploy'
+os.system(f'git clone https://code.openxlab.org.cn/q4171119/lesson-lmdeploy.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+pipe = pipeline(base_path,
+                backend_config=backend_config)
+
+gen_config = GenerationConfig(top_p=0.8,
+                              top_k=40,
+                              temperature=0.8,
+                              max_new_tokens=1024)
+       
+def chat(message,history):
+    response = pipe(message,
+                    gen_config = gen_config)
+    return response.text
+
+demo = gr.ChatInterface(
+            fn = chat,
+            title="InternLM2-Chat-1.8_4bit-self-cognition",
+            description="""InternLM is mainly developed by Shanghai AI Laboratory.  """,
+            )
+demo.queue(1).launch()
